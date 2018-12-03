eff28d931a96d543be9dc348710703c8286cb80c
mamo <mamo0245@colorado.edu>
2018-12-02 21:48:26 -0700

    formatted

--- questionnaire.js
+++ questionnaire.js
@@ -1,1 +1,13 @@
 
+// Below function Executes on click of submit button.
+function submit()
+{
+	var name = document.getElementById("name").value;
+	var username = document.getElementById("username").value;
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("pwd").value;
+	var birthday = document.getElementById("dob").value;
+	var age = document.getElementById("age").value;
+	var gender = document.getElementById("gender").value;
+	
+}
