1809442b4b928003323bec24c15ff2c70a21f787
mamo <mamo0245@colorado.edu>
2018-12-02 22:09:49 -0700

    incorrect ids: fixed

--- questionnaire.js
+++ questionnaire.js
@@ -10,4 +10,16 @@ function submit()
 	var age = document.getElementById("age").value;
 	var gender = document.getElementById("gender").value;
 	
+	var q1 =  document.getElementById("methodology").value;
+	var q2 =  document.getElementById("certified").value;
+	var q3 =  document.getElementById("artist").value;
+	var q4 =  document.getElementById("roles").value;
+	var q5 =  document.getElementById("activity").value;
+	var q6 =  document.getElementById("certifiedage").value;
+	var q7 =  document.getElementById("language").value;
+	var q8 =  document.getElementById("sprints").value;
+	var q9 =  document.getElementById("scenario1").value;
+	var q10 =  document.getElementById("scenario2").value;
+	
+	
 }
