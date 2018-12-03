896aa40451589bbd99cfacc1add62fbaa4a61db9
Bridget Murphy <Bridget@engr2-18-87-dhcp.int.colorado.edu>
2018-12-02 19:48:49 -0700

    Added first draft of matching.js

---
+++ matching.js
@@ -0,0 +1,36 @@
+function CalculateScore(answer_set){
+	var score;
+	for(i=0;i<answer_set.length;i++){
+		if(answers[i]=="a"){
+			score += 1;
+		}
+		else if(answers[i]=="b"){
+			score += 2;
+		}
+		else if(answers[i]=="c"){
+			score += 3;
+		}
+		else{
+			score += 4;
+		}
+	}
+	return score
+}
+
+function match(score,answer_set){
+	var ratio;
+	var status;
+	var matches = [];
+	for(i=0;i<users.length;i++){
+		if(score >= answer_set.score){
+			ratio = score/answer_set.score;
+		}
+		else{
+			ratio = answer_set.score/score;
+		}
+
+		if(ratio >= 0.8){
+			matches.append(answer_set.id);
+		}
+	}
+}
