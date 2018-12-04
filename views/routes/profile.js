169db11b5924347d5b7da322ebb0d52bce18db4a
Chase Dudas <chasedudas13@gmail.com>
2018-12-04 09:32:18 -0700

    Add files via upload

---
+++ views/routes/profile.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var db = require('../database');
+var app = express();
+module.exports = app;
+
+app.get('/', function (request, response) {
+
+    // TODO: Initialize the query variable with a SQL query
+    // that returns all the rows in the ‘store’ table
+    var query = 'SELECT * FROM users';
+
+    db.any(query)
+        .then(function (rows) {
+            // render views/store/list.ejs template file
+            response.render('profile/list', {
+                title: 'Profile',
+                data: rows
+            })
+        })
+        .catch(function (err) {
+            // display error message in case an error
+            request.flash('error', err);
+            response.render('profile/list', {
+                title: 'Profile',
+                data: ''
+            })
+        })
+});
