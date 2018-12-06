452aba0a9725d7cb0f7b0d6f1f5cf5300e248b60
Bridget Murphy <Bridget@engr2-18-87-dhcp.int.colorado.edu>
2018-12-06 12:05:09 -0700

    version 2.0

--- routes/matches.js
+++ routes/matches.js
@@ -2,125 +2,79 @@ var express = require('express');
 var db = require('../database');
 var app = express();
 module.exports = app;
-
-app.get('/', function (request, response) {
-
-    // TODO: Initialize the query variable with a SQL query
-    // that returns all the rows and columns in the 'store' table
-    var query = 'SELECT match1,match2,match3,match4,match5,match6,match7,match8,match9,match10 FROM matches';
-
-    db.any(query)
-      .then(function (rows) {
-          // render views/store/list.ejs template file
-          response.render('matches/list', {
-              title: 'Matches listing',
-              data: rows
-          })
-      })
-      .catch(function (err) {
-          // display error message in case an error
-          request.flash('error', err);
-          response.render('matches/list', {
-              title: 'Matches listing',
-              data: ''
-          })
-      })
-});
-
-/*app.get('/add', function (request, response) {
-    // render views/matches/add.ejs
-    response.render('matches/add', {
-        title: 'Add New Item',
-        sname: '',
-        qty: '',
-        price: ''
-    })
+/*app.get('/', function (request, response) {
+    // render views/store/add.ejs
+    response.render('matches/list',{
+	title: 'insert username to view matches',
+	user_name: '',})
 });*/
-
 // Route to insert values. Notice that request method is POST here
-app.post('/add', function (request, response) {
-    function CalculateScore(){
-        var score;
-        for(i=1;i<VALUES.length-1;i++){
-            if(VALUES[i]=="a"){
-                score += 1;
-            }
-            else if(VALUES[i]=="b"){
-                score += 2;
-            }
-            else if(VALUES[i]=="c"){
-                score += 3;
-            }
-            else{
-                score += 4;
-            }
-        }
-        return score;
-    }
-
-    function match(){
-        var ratio;
-        var status;
-        var matches = [];
-        var score = CalculateScore(users.id)
-        for(i=0;i<users.length;i++){
-            if(score >= answer_set.score){
-                ratio = score/answer_set.score;
-            }
-            else{
-                ratio = answer_set.score/score;
-            }
-
-            if(ratio >= 0.8){
-                INSERT INTO MATCHES VALUES id
-            }
-        }
-    }
-
+app.get('/', function (request, response) {
     // Validate user input - ensure non emptiness
-    request.assert('full_name', 'name is required').notEmpty();
-    //request.assert('qty', 'Quantity is required').notEmpty();
-    //request.assert('price', 'Price is required').notEmpty();
-
-    var errors = request.validationErrors();
+    //user_name = 'chase13';
+    //request.assert('user_name', 'User name is required').notEmpty();
+    //var errors = request.validationErrors();
+    var errors = false;
     if (!errors) { // No validation errors
         var item = {
             // sanitize() is a function used to prevent Hackers from inserting
             // malicious code(as data) into our database. There by preventing
             // SQL-injection attacks.
-            full_name: request.sanitize('full_name').escape().trim(),
-            email: request.sanitize('email').escape().trim(),
-            //price: request.sanitize('price').escape().trim()
+            //user_name: request.sanitize('user_name').escape().trim()
         };
         // Running SQL query to insert data into the store table
-        db.none('INSERT INTO matches(name, email) VALUES($1, $2, $3)', [users.full_name,users.email])
+        db.any('SELECT id,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10 FROM answer_set')
+        	.then(function(results){
+        		//request.flash('success', 'Successfully took user name!');
+        		console.log(results)
+        		response.render('matches/list',{
+        			title: 'matches',
+        			data: results,
+        			username: 'chase13',
+        			id: '1'
+        		})	
+        	})
+        .catch(function(err){
+        	response.render('matches/list',{
+        		title: 'matches'
+        	})
+        })
+}
+
+        /*db.none('INSERT INTO users(full_name, user_name, date_of_birth, age, email, password, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $15, $16)', [item.full_name, item.user_name, item.date_of_birth, item.age, item.email, item.password, item.q1, item.q2, item.q3, item.q4, item.q5, item.q6, item.q7, item.q8, item.q9, item.q10])
             .then(function (result) {
-                request.flash('success', 'Data added successfully!');
-                // render views/store/add.ejs
-                response.render('matches/add', {
-                    title: 'Add New Item',
-                    sname: '',
-                    qty: '',
-                    price: ''
-                })
+                request.flash('success', 'Successfully created a user!');
+                // render to profile
+                response.render('questionnaire/questionnaire',{
+       		title: 'questionnaire',
+        	full_name: '',
+        	user_name: '',
+        	date_of_birth: '',
+        	age: '',
+        	email: '',
+        	password: '',})
             }).catch(function (err) {
             request.flash('error', err);
             // render views/store/add.ejs
-            response.render('matches/add', {
-                title: 'Add New Item',
-                sname: item.sname,
-                qty: item.qty,
-                price: item.price
-            })
-        })
-    } else {
+            response.render('questionnaire/questionnaire',{
+	        title: 'questionnaire',
+       	 	full_name: '',
+      	 	user_name: '',
+     	  	date_of_birth: '',
+      	  	age: '',
+        	email: '',
+        	password: '',})
+	})
+	}*/
+     else {
         var error_msg = errors.reduce((accumulator, current_error) => accumulator + '<br />' + current_error.msg, '');
         request.flash('error', error_msg);
-        response.render('matches/add', {
-            title: 'Add New Item',
-            sname: request.body.sname,
-            qty: request.body.qty,
-            price: request.body.price
-        })
+        response.render('matches/list',{
+        title: 'matches',
+        user_name: ''
+})
+        
     }
 });
+
+//app.post here
