e893aaedbd590149addde9609954e0b60eb61400
Chase Dudas <chasedudas13@gmail.com>
2018-12-09 22:38:16 -0700

    Add files via upload

--- test/test.js
+++ test/test.js
@@ -7,10 +7,69 @@ describe('Basic Functionality', function()
 {
 	describe('Server.js', function()
 	{
-		it('Should respond with hello world', function() 
+		it('Handles a GET request', function() 
   		{
-    		request(app).get('/').expect('hello world');
+    		request(app).get('/').expect('');
+  		})
+  		it('Handles a POST request', function() 
+  		{
+    		request(app).post('/').expect('');
+  		})
+  	})
+	describe('Index.js', function()
+	{
+		it('Handles a GET request', function()
+		{
+			request(app).get('/index').expect('');
+		})
+		it('Handles a POST request', function() 
+  		{
+    		request(app).post('/index').expect('');
+  		})
+	})
+	describe('Matches.js', function()
+	{
+		it('Handles a GET request', function()
+		{
+			request(app).get('/matches').expect('');
+		})
+		it('Handles a POST request', function() 
+  		{
+    		request(app).post('/matches').expect('');
+  		})
+	})
+	describe('Profile.js', function()
+	{
+		it('Handles a GET request', function()
+		{
+			request(app).get('/profile').expect('');
+		})
+		it('Handles a POST request', function() 
+  		{
+    		request(app).post('/profile').expect('');
+  		})
+	})
+	describe('Login.js', function()
+	{
+		it('Handles a GET request', function()
+		{
+			request(app).get('/login').expect('');
+		})
+		it('Handles a POST request', function() 
+  		{
+    		request(app).post('/login').expect('');
+  		})
+	})
+	describe('Questionnaire.js', function()
+	{
+		it('Handles a GET request', function()
+		{
+			request(app).get('/questionnaire').expect('');
+		})
+		it('Handles a POST request', function() 
+  		{
+    		request(app).post('/questionnaire').expect('');
+  		})
+	})
 
-  		});
-	});
 });
