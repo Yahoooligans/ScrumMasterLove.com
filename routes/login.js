b6bd0669dbc6fff7b3acb14bbdb07421645cca28
Chase Dudas <chasedudas13@gmail.com>
2018-12-11 10:48:57 -0700

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,25 @@ var express = require('express');
 var db = require('../database');
 var app = express();
 
-app.post('/', function (req, response) 
+app.post('/', function (req, res) 
 {
+  var sid = req.sessionID;
+  var username = req.body.user;
+  var password = req.body.pass;
+
+  var queryUser = 'SELECT * FROM users WHERE'
+
+  db.one(queryUser)
+  .then(function (row) {
+    
+  })
+  .catch(function (err) {
+    // display error message in case an error
+    request.flash('error', err);
+  })
+  
   // Process the data received in req.body
-  response.redirect('/');
+  res.redirect('/');
 });
 
 module.exports = app;
