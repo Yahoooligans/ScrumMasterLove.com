35c5fa8aaa31cf5ad3fdf8b0dd7b9395bf7775cf
Bridget Murphy <39529905+brmu8861@users.noreply.github.com>
2018-12-11 10:55:14 -0700

    Update matches.js
    Now reads from user database

--- routes/matches.js
+++ routes/matches.js
@@ -2,27 +2,60 @@ var express = require('express');
 var db = require('../database');
 var app = express();
 module.exports = app;
-
+/*app.get('/', function (request, response) {
+    // render views/store/add.ejs
+    response.render('matches/list',{
+    title: 'insert username to view matches',
+    user_name: '',})
+});*/
+// Route to insert values. Notice that request method is POST here
 app.get('/', function (request, response) {
-
-    // TODO: Initialize the query variable with a SQL query
-    // that returns all the rows in the ‘store’ table
-    var query = 'SELECT * FROM users';
-
-    db.any(query)
-        .then(function (rows) {
-            // render views/store/list.ejs template file
-            response.render('matches/list', {
-                title: 'Matches',
-                data: rows
+    // Validate user input - ensure non emptiness
+    //user_name = 'chase13';
+    //request.assert('user_name', 'User name is required').notEmpty();
+    //var errors = request.validationErrors();
+    var errors = false;
+    if (!errors) { // No validation errors
+        var item = {
+            // sanitize() is a function used to prevent Hackers from inserting
+            // malicious code(as data) into our database. There by preventing
+            // SQL-injection attacks.
+            //user_name: request.sanitize('user_name').escape().trim()
+        };
+        // Running SQL query to insert data into the store table
+        db.any('SELECT * FROM users')
+            .then(function(results){
+                //request.flash('success', 'Successfully took user name!');
+                console.log(results)
+                response.render('matches/list',{
+                    title: 'matches',
+                    data: results,
+                    username: 'chase13',
+                    id: '1'
+                })  
             })
-        })
-        .catch(function (err) {
-            // display error message in case an error
-            request.flash('error', err);
-            response.render('matches/list', {
-                title: 'Matches',
+        .catch(function(err){
+            response.render('matches/list',{
+                title: 'matches',
                 data: ''
             })
         })
+}
+
+  
+     else {
+        var error_msg = errors.reduce((accumulator, current_error) => accumulator + '<br />' + current_error.msg, '');
+        request.flash('error', error_msg);
+        response.render('matches/list',{
+        title: 'matches',
+        user_name: ''
+})
+        
+    }
+});
+
+//app.post here
+
+app.post('/', function (req, res) {
+  res.send('POST request to homepage');
 });
