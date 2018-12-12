3e78b7210611b0424bd0a2f27185b66e32750baf
Chase Dudas <chasedudas13@gmail.com>
2018-12-11 20:52:25 -0700

    Update mysqlfile.sql

--- mysqlfile.sql
+++ mysqlfile.sql
@@ -35,7 +35,7 @@ CREATE TABLE "users" (
   "matches_id" integer REFERENCES matches(id),
   "full_name" varchar, 
   "email" varchar UNIQUE,
-  "user_name" text UNIQUE,
+  "user_name" varchar UNIQUE,
   "password" varchar UNIQUE,
   "gender" varchar,
   "age" varchar,
