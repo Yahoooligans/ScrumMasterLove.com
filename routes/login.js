4cb16d0228f3aaf06b95acadf599e5dacf724054
Chase Dudas <chasedudas13@gmail.com>
2018-12-12 20:35:03 -0700

    Updated login the verifies users

--- routes/login.js
+++ routes/login.js
@@ -4,28 +4,27 @@ var app = express();
 
 app.post('/', function (req, res) 
 {
+  //Recieves text data from the form in the navbar found in views/layouts/header
   var username = req.body.username;
   var password = req.body.psw;
-
-  //console.log(username);
-  //console.log(password);
 
   //Previous values, in case the user enters the wrong info 
   var oldusername = this.username;
   var oldpassword = this.password;
 
+  //Sets the global variables equal to the ones recieved in the form
   this.username = username;
   this.password = password;
-  //console.log(this.username);
-  //console.log(this.password);
+ 
+  //Searches for any users in the database that have the same username and password.
+  //On error, the previous username and email are restored to the global variables
+  var query = "SELECT * FROM users WHERE user_name = '" + this.username + "' AND password = '" + this.password + "'" ;
 
-  var query = "SELECT * FROM users WHERE user_name = '" + this.username + "'" ;
-
-  //If username is in the table then continue, if not error
+  //If username is in the table and the password is correct then continue, if not error
    db.one(query)
         .then(function(results)
         {
-          console.log(results);
+          //redirect to the homepage
           res.redirect('/'); 
         })
         .catch(function(error)
@@ -35,11 +34,6 @@ app.post('/', function (req, res)
           console.log("Error quering the database for user");
           res.redirect('/');
         })
-
-  //If username is in table and password matches redirect and success, else error 
-  
-  // Process the data received in req.body
-  //res.redirect('/');
 });
 
 module.exports = app;
