ab5aa33eb46169c0c8b778fca0bd0becf285770f
Chase Dudas <chasedudas13@gmail.com>
2018-12-12 21:26:57 -0700

    Add files via upload

--- database.js
+++ database.js
@@ -1,13 +1,6 @@
 var pgp = require('pg-promise')();
 
-const dbConfig = {
-   host: 'localhost',
-   port: 5432,
-   database: 'scrumlove',
-   user: 'chasedudas',
-   password: 'Chase13' // TODO: Fill in your PostgreSQL password here.
-                // Use empty string if you did not set a password
-};
+const dbConfig = process.env.DATABASE_URL;
 
 var db = pgp(dbConfig);
 
